c4336d2fbee61bbb5c737110c537960ff3a27b61
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2018-07-04 10:54:13 -0700

    Update reversearray_minmemory.py

--- reversearray_minmemory.py
+++ reversearray_minmemory.py
@@ -1,13 +1,4 @@
-#reverse array without using reverse_function
-def reverse_array(array):
-	length = len(array)
-	mid_size = length/2
-	for i in range(0,int(mid_size)):
-		store_length = length - i -1
-		temp = array[store_length]
-		array[store_length] = array[i]
-		array[i] = temp
-	return [array];
+
 #makes no use of temporary variable most memory_efficient
 def reverse_array_no_tempvariable(nums):
 	length = len(nums)

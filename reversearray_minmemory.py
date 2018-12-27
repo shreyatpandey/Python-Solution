9d77f9b93932cf74653b3009db4bead0f829ffd2
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2018-12-27 18:02:15 +0530

    Update reversearray_minmemory.py

--- reversearray_minmemory.py
+++ reversearray_minmemory.py
@@ -8,6 +8,11 @@ def reverse_array_no_tempvariable(nums):
 		nums[length-i-1] = nums[i] - nums[length-i-1]
 		nums[i] = nums[i]-nums[length-i-1]
 	return nums;
+def xor_number(x,y):
+	x = x^y
+	y = x^y
+	x = x^y
+	return x,y ;
 	
 
 
@@ -15,6 +20,10 @@ list = reverse_array([2,4,6,1])
 list1 = reverse_array_no_tempvariable([2,4,6,1])
 print(list)
 print(list1)
+x,y = xor_number(15,10)
+
+
+
 
 
 

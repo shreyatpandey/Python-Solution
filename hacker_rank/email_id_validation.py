bbd69f3cabe63bcc883d2c3d902af714242a98e7
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-06-02 23:04:16 -0700

    Update email_id_validation.py

--- hacker_rank/email_id_validation.py
+++ hacker_rank/email_id_validation.py
@@ -1,3 +1,16 @@
+'''
+You must print each valid email address in the same order as it was received as input.
+
+Sample Input
+
+2  
+DEXTER <dexter@hotmail.com>
+VIRUS <virus!@variable.:p>
+Sample Output
+
+DEXTER <dexter@hotmail.com>
+'''
+
 import re
 number_of_email_address = int(input())
 for i in range(0,number_of_email_address):

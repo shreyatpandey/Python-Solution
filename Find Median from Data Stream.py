1bc5ab8ca7df0c1f421c3cf6f1a65f7a84fe3618
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-10-19 22:29:21 -0700

    Find Median from Data Stream

--- Find Median from Data Stream.py
+++ Find Median from Data Stream.py
@@ -29,3 +29,31 @@ class MedianFinder(object):
         else:
             return self.list_median[length//2]
         
+'''
+Heap Approach
+'''
+from heapq import *
+class MedianFinder(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.heaps = None, [], []
+        self.i = 1
+
+    def addNum(self, num):
+        """
+        :type num: int
+        :rtype: None
+        """
+        heappush(self.heaps[-self.i], -heappushpop(self.heaps[self.i], num * self.i))
+        self.i *= -1
+        
+
+    def findMedian(self):
+        """
+        :rtype: float
+        """
+        return (self.heaps[self.i][0] * self.i - self.heaps[-1][0]) / 2.0
+        

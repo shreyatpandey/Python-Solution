f30a4eefbd2350cba732205cc67bbd3497629b33
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-12-22 18:53:51 -0800

    Update Longest Common Subsequence.py

--- dp/Longest Common Subsequence.py
+++ dp/Longest Common Subsequence.py
@@ -20,26 +20,26 @@ class Solution(object):
     
     #Bottom-Up Fashion | Memoization
     class Solution(object):
-    def longestCommonSubsequence(self, text1, text2):
+        def longestCommonSubsequence(self, text1, text2):
         """
         :type text1: str
         :type text2: str
         :rtype: int
         """
-        lengthtext1 = len(text1) 
-        lengthtext2 = len(text2) 
-  
-        LCS = [[]]
-        LCS = [[0]*(lengthtext2+1) for i in range(lengthtext1+1)] 
-        for i in range(lengthtext1+1): 
-            for j in range(lengthtext2+1): 
-                if i == 0 or j == 0 : 
-                    LCS[i][j] = 0
-                elif text1[i-1] == text2[j-1]: 
-                    LCS[i][j] = LCS[i-1][j-1]+1
-                else: 
-                    LCS[i][j] = max(LCS[i-1][j] , LCS[i][j-1]) 
-   
-        return LCS[lengthtext1][lengthtext2] 
-        
-   
+            lengthtext1 = len(text1) 
+            lengthtext2 = len(text2) 
+
+            LCS = [[]]
+            LCS = [[0]*(lengthtext2+1) for i in range(lengthtext1+1)] 
+            for i in range(lengthtext1+1): 
+                for j in range(lengthtext2+1): 
+                    if i == 0 or j == 0 : 
+                        LCS[i][j] = 0
+                    elif text1[i-1] == text2[j-1]: 
+                        LCS[i][j] = LCS[i-1][j-1]+1
+                    else: 
+                        LCS[i][j] = max(LCS[i-1][j] , LCS[i][j-1]) 
+
+            return LCS[lengthtext1][lengthtext2] 
+
+

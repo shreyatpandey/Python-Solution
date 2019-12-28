dac8a3092a3641a9182d3d1e0c7f6a84bb0aa803
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-12-27 16:45:22 -0800

    Update Coin Change.py

--- dp/Coin Change.py
+++ dp/Coin Change.py
@@ -24,5 +24,34 @@ class Solution(object):
                     dp[change] = min(dp[change],dp[change-coins[coin]]+1)
         
         return dp[-1] if dp[-1] != float('inf') else -1
-                    
-        
+
+'''
+Follow Up Question: Return the list of coins as well as the list of coins
+'''
+from itertools import combinations_with_replacement
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [float('inf')* i for i in range(amount+1)]
+        dp[0] = 0
+        listofcoins = []
+        for change in range(1,amount+1):
+            for coin in range(0,len(coins)):
+                if coins[coin]<= change :
+                    dp[change] = min(dp[change],dp[change-coins[coin]]+1)
+            
+        coinscombination = list(combinations_with_replacement(coins,dp[change]))
+        for i in range(0,len(coinscombination)):
+            calamount = 0
+            for j in range(0,len(coinscombination[i])):
+                calamount += coinscombination[i][j]
+                if amount == calamount :
+                    listofcoins = list(coinscombination[i])
+        if dp[-1] != float('inf') :
+            return dp[-1],listofcoins
+        else:
+            return -1,[]

d856b8593f574cc82e1fca50051993cca28fd3a6
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2020-06-20 19:12:29 -0700

    Added Deque Approach

--- Tree/N-ary Tree Level Order Traversal.py
+++ Tree/N-ary Tree Level Order Traversal.py
@@ -34,5 +34,37 @@ class Solution(object):
                 i += 1
             level += 1
         return NaryTreeLevelOrderTraversal
+
+"""
+Approach-2: Using Deque
+Runtime: 99.72%
+"""
+    
+from collections import deque
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: Node
+        :rtype: List[List[int]]
+        """
+        Queue = deque([root])
+        NaryTreeLevelOrderTraversal = []
+        if not root:
+            return NaryTreeLevelOrderTraversal
+        level = 0
+        while len(Queue) != 0:
+            NaryTreeLevelOrderTraversal.append([])
+            LenQueue = len(Queue)
+            i = 0
+            while i < LenQueue :
+                node = Queue.popleft()
+                NaryTreeLevelOrderTraversal[level].append(node.val)
+                if node.children :
+                    Queue += node.children
+                i += 1
+            level += 1
+        return NaryTreeLevelOrderTraversal
+        
+                        
         
                         

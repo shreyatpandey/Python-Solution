6d249def0e23e2337e04b4d1107943995c52770b
Shreyat <arjenbhar@gmail.com>
2020-10-28 18:19:43 -0700

    Removed All Files

--- Python/Defanging an IP Address.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def defangIPaddr(self, address):
-        """
-        :type address: str
-        :rtype: str
-        """
-        string_new = address.replace(".","[.]")
-        return string_new
-        
--- Python/Reverse Words in a String.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def reverseWords(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        list_split_string = s.split()
-        return (' '.join(reversed(list_split_string)))
